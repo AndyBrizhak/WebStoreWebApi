f53f224e97e6387682bd5d416f08a6d474fdd29c
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-23 12:44:13 +0800

    Data projections between models and DTOs

--- Services/WebStore.Services/Mapping/ProductMapper.cs
+++ Services/WebStore.Services/Mapping/ProductMapper.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using WebStore.Domain.Entities;
 using WebStore.Domain.ViewModels;
+using WebStore.Domain.DTO.Products;
 
 namespace WebStore.Services.Mapping
 {
@@ -18,5 +19,27 @@ namespace WebStore.Services.Mapping
         };
 
         public static IEnumerable<ProductViewModel> ToView(this IEnumerable<Product> products) => products.Select(ToView);
+
+        public static ProductDTO ToDTO(this Product product) => product is null ? null : new ProductDTO
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Order = product.Order,
+            Price = product.Price,
+            ImageUrl = product.ImageUrl,
+            Brand = product.Brand.ToDTO(),
+            Section = product.Section.ToDTO(),
+        };
+
+        public static Product FromDTO(this ProductDTO product) => product is null ? null : new Product
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Order = product.Order,
+            Price = product.Price,
+            ImageUrl = product.ImageUrl,
+            Brand = product.Brand.FromDTO(),
+            Section = product.Section.FromDTO(),
+        };
     }
 }
