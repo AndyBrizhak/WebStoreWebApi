83674eb12241c496fbbe82b0c3db696f3efed3d9
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-23 13:52:00 +0800

    Improvement of the IPoductData interface to use DTO-models in it

--- Services/WebStore.Interfaces/Service/IProductData.cs
+++ Services/WebStore.Interfaces/Service/IProductData.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections.Generic;
 using WebStore.Domain;
+using WebStore.Domain.DTO.Products;
 using WebStore.Domain.Entities;
 
 namespace WebStore.Interfaces.Service
 {
     public interface IProductData
     {
-        IEnumerable<Section> GetSections();
+        //IEnumerable<Section> GetSections();
 
-        IEnumerable<Brand> GetBrands();
+        //IEnumerable<Brand> GetBrands();
 
-        IEnumerable<Product> GetProducts(ProductFilter Filter = null);
+        //IEnumerable<Product> GetProducts(ProductFilter Filter = null);
 
-        Product GetProductById(int id);
+        //Product GetProductById(int id);
+
+        IEnumerable<SectionDTO> GetSections();
+        IEnumerable<BrandDTO> GetBrands();
+        IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null);
+        ProductDTO GetProductById(int id);
     }
 }
