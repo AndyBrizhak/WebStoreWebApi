a332ba160524a5abc475c55a4207380b4b140073
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-26 12:44:04 +0800

    Refinement of base client asynchronous operations

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 using System.Net.Http;
-using Microsoft.Extensions.Configuration;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace WebStore.Clients.Base
 {
-    /// <summary>
-    /// Class for create Clients
-    /// </summary>
     public abstract class BaseClient : IDisposable
     {
         protected readonly string _ServiceAddress;
         protected readonly HttpClient _Client;
 
-        protected BaseClient(IConfiguration Configuration, string ServiceAddress )
+        protected BaseClient(IConfiguration Configuration, string ServiceAddress)
         {
-            //_Configuration = Configuration;
             _ServiceAddress = ServiceAddress;
+
             _Client = new HttpClient
             {
                 BaseAddress = new Uri(Configuration["WebApiURL"]),
@@ -30,36 +28,36 @@ namespace WebStore.Clients.Base
 
         protected T Get<T>(string url) => GetAsync<T>(url).Result;
 
-        protected async Task<T> GetAsync<T>(string url)
+        protected async Task<T> GetAsync<T>(string url, CancellationToken Cancel = default)
         {
-            var response = await _Client.GetAsync(url);
+            var response = await _Client.GetAsync(url, Cancel);
             return await response
-                .EnsureSuccessStatusCode() // Убеждаемся, что в ответ получен 200-ый статусный код.
-                .Content             // В ответе есть содержимое с которым можно работать
-                .ReadAsAsync<T>(); // Десериализуем данные в нужный тип объекта
+               .EnsureSuccessStatusCode() // Убеждаемся, что в ответ получен 200-ый статусный код.
+               .Content             // В ответе есть содержимое с которым можно работать
+               .ReadAsAsync<T>(Cancel); // Десериализуем данные в нужный тип объекта
         }
 
         protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
 
-        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item, CancellationToken Cancel = default)
         {
-            var response = await _Client.PostAsJsonAsync(url, item);
+            var response = await _Client.PostAsJsonAsync(url, item, Cancel);
             return response.EnsureSuccessStatusCode();
         }
 
         protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
 
-        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item, CancellationToken Cancel = default)
         {
-            var response = await _Client.PutAsJsonAsync(url, item);
+            var response = await _Client.PutAsJsonAsync(url, item, Cancel);
             return response.EnsureSuccessStatusCode();
         }
 
         protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
 
-        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken Cancel = default)
         {
-            var response = await _Client.DeleteAsync(url);
+            var response = await _Client.DeleteAsync(url, Cancel);
             return response;
         }
 
