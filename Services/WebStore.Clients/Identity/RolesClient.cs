3ccdff0222f98f12abecf63012989acf6b51d88e
Андрей Брижак <andrewbrizhak@gmail.com>
2020-09-26 13:06:45 +0800

    WebAPI client for user management

--- Services/WebStore.Clients/Identity/RolesClient.cs
+++ Services/WebStore.Clients/Identity/RolesClient.cs
@@ -1,10 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces.Service.Identity;
 
 namespace WebStore.Clients.Identity
 {
-    class RolesClient
+    public class RolesClient : BaseClient, IRolesClient
     {
+        public RolesClient(IConfiguration Configuration) : base(Configuration, WebAPI.Identity.Roles) { }
+
+
+        public async Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
+
+        public async Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
+
+        public async Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
+
+        public async Task<string> GetRoleIdAsync(Role role, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
+
+        public async Task<string> GetRoleNameAsync(Role role, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
+
+        public async Task SetRoleNameAsync(Role role, string roleName, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
+
+        public async Task<string> GetNormalizedRoleNameAsync(Role role, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
+
+        public async Task SetNormalizedRoleNameAsync(Role role, string normalizedName, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
+
+        public async Task<Role> FindByIdAsync(string roleId, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
+
+        public async Task<Role> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken) { throw new System.NotImplementedException(); }
     }
 }
