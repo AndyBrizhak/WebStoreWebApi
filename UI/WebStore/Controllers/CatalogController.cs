c78a950bf51e3b63a4335862fe54e7e5a855a453
Андрей Брижак <andrewbrizhak@gmail.com>
2020-10-10 11:31:44 +0800

    Refinement of the product catalog filter for paginated splitting

--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using WebStore.Domain;
 using WebStore.Domain.ViewModels;
 using WebStore.Interfaces.Service;
+using WebStore.Interfaces.Services;
 using WebStore.Services.Mapping;
 
 namespace WebStore.Controllers
@@ -10,15 +12,26 @@ namespace WebStore.Controllers
     public class CatalogController : Controller
     {
         private readonly IProductData _ProductData;
+        private readonly IConfiguration _Configuration;
 
-        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
-
-        public IActionResult Shop(int? BrandId, int? SectionId)
+        public CatalogController(IProductData ProductData, IConfiguration Configuration)
         {
+            _ProductData = ProductData;
+            _Configuration = Configuration;
+        }
+
+        public IActionResult Shop(int? BrandId, int? SectionId, int Page = 1)
+        {
+            var page_size = int.TryParse(_Configuration["PageSize"], out var size)
+                ? size
+                : (int?)null;
+
             var filter = new ProductFilter
             {
                 BrandId = BrandId,
-                SectionId = SectionId
+                SectionId = SectionId,
+                Page = Page,
+                PageSize = page_size
             };
 
             var products = _ProductData.GetProducts(filter);
